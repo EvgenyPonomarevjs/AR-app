406c50da3daa74b1da5bc3a3ff4fbbfef9a1f786
Евгений Пономарев <ponomareve45@gmail.com>
2022-11-09 11:08:56 +0300

    BagFix

--- js/log.js
+++ js/log.js
@@ -5,5 +5,5 @@ const puck = document.getElementById('puck');
 const hockeyStick = document.getElementById('hockeyStick')
 
 
-puck.setAttribute("url", "https://raw.githubusercontent.com/EvgenyPonomarevjs/Base-qr-code/main/Base%20qr-code/Pattern/pattern-3.patt");
-console.log(puck)
+// puck.setAttribute("url", "https://raw.githubusercontent.com/EvgenyPonomarevjs/Base-qr-code/main/Base%20qr-code/Pattern/pattern-3.patt");
+// console.log(puck)
