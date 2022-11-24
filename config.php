0eab694df2f00fbddcd6db72e77d19973804b262
Евгений Пономарев <ponomareve45@gmail.com>
2022-11-24 14:06:04 +0300

    added the wex admin panel

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+//! Be sure to change your password!
+// Login
+$GLOBALS['admin_name'] = 'admin';
+
+// Password
+$GLOBALS['admin_password'] = '1590';
+
+//! Edit fields below for personal settings
+// Default language: en & ru
+$GLOBALS['default_language'] = 'ru';
+
+// Default theme: white & black
+$GLOBALS['default_theme'] = 'white';
+
+//! Development settings
+// Default code theme: elegant & twilight
+$GLOBALS['default_code_theme'] = 'elegant';
+
+// Show tags? <h1> </h1> ... .
+// Default: no & yes
+$GLOBALS['default_tags_show'] = 'no';
